3c0c5996153db19c8100fd2dc09edcebd77567b3
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-06-27 14:05:26 +0500

    good first approaches added

--- components/faq.js
+++ components/faq.js
@@ -2,82 +2,82 @@ import React from "react";
 import Container from "./container";
 import { Disclosure } from "@headlessui/react";
 const Faq = () => {
-  const faqdata = [
-    {
-      question: "What services do you offer?",
-      answer:
+	const faqdata = [
+		{
+			question: "What services do you offer?",
+			answer:
         "We offer a wide range of services including web development, mobile app development, UI/UX design, and digital marketing.",
-    },
-    {
-      question: "How can I request a quote?",
-      answer:
+		},
+		{
+			question: "How can I request a quote?",
+			answer:
         "To request a quote, you can fill out the contact form on our website or send us an email with your project requirements.",
-    },
-    {
-      question: "What is your pricing structure?",
-      answer:
+		},
+		{
+			question: "What is your pricing structure?",
+			answer:
         "Our pricing is tailored to each project based on its complexity, scope, and specific requirements. We provide detailed pricing information after reviewing the project details.",
-    },
-    {
-      question: "Do you provide ongoing maintenance and support?",
-      answer:
+		},
+		{
+			question: "Do you provide ongoing maintenance and support?",
+			answer:
         "Yes, we offer ongoing maintenance and support services to ensure your website or application continues to run smoothly and stays up-to-date.",
-    },
-  ];
+		},
+	];
 
-  return (
-    <Container className="!p-0">
-      <div className="w-full max-w-2xl p-2 mx-auto rounded-2xl">
-        {faqdata.map((item, index) => (
-          <div key={item.question} className="mb-5">
-            <Disclosure>
-              {({ open }) => (
-                <>
-                  <Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200">
-                    <span>{item.question}</span>
-                    {open ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                      </svg>
-                    )}
-                  </Disclosure.Button>
-                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-500 dark:text-gray-300 transition-all">
-                    {item.answer}
-                  </Disclosure.Panel>
-                </>
-              )}
-            </Disclosure>
-          </div>
-        ))}
-      </div>
-    </Container>
-  );
+	return (
+		<Container className="!p-0">
+			<div className="w-full max-w-2xl p-2 mx-auto rounded-2xl">
+				{faqdata.map((item, index) => (
+					<div key={index} className="mb-5">
+						<Disclosure>
+							{({ open }) => (
+								<>
+									<Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200">
+										<span>{item.question}</span>
+										{open ? (
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth="1.5"
+												stroke="currentColor"
+												className="w-6 h-6"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M6 18L18 6M6 6l12 12"
+												/>
+											</svg>
+										) : (
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth="1.5"
+												stroke="currentColor"
+												className="w-6 h-6"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M12 4.5v15m7.5-7.5h-15"
+												/>
+											</svg>
+										)}
+									</Disclosure.Button>
+									<Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-500 dark:text-gray-300 transition-all">
+										{item.answer}
+									</Disclosure.Panel>
+								</>
+							)}
+						</Disclosure>
+					</div>
+				))}
+			</div>
+		</Container>
+	);
 };
 
 export default Faq;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider attribute="class">
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
 export default MyApp;
