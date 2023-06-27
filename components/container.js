b2898609c74becfa80d1e880b2ad6f4bc8f1c320
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-06-27 15:09:09 +0500

    lint test

--- components/container.js
+++ components/container.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Container = (props) => {
-  return (
-    <div
-      id={props?.id}
-      className={`container p-8 mx-auto md:px-4 xl:px-4 ${
-        props.className ? props.className : ""
-      }`}
-    >
-      {props.children}
-    </div>
-  );
+	return (
+		<div
+			id={props?.id}
+			className={`container p-8 mx-auto md:px-4 xl:px-4 ${
+				props.className ? props.className : ""
+			}`}
+		>
+			{props.children}
+		</div>
+	);
 };
 
 export default Container;
