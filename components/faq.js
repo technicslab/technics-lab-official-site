e50fca8431cfb153a55c5cb7e8be42f2513c0aa5
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-07-02 02:03:07 +0500

    fixes with scroll and faq and images

--- components/faq.js
+++ components/faq.js
@@ -1,89 +1,102 @@
 import React from "react";
 import Container from "./container";
 import { Disclosure } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
+
 const Faq = () => {
-	const faqData = [
-		{
-			question: "What services do you offer?",
-			answer:
-        "We offer a wide range of services including web development, mobile app development, Smart Contract with Blockchain , and Software Prototyping",
-		},
-		{
-			question: "How can I request a quote?",
-			answer:
+  const faqData = [
+    {
+      question: "What services do you offer?",
+      answer:
+        "We offer a wide range of services including web development, mobile app development, Smart Contract with Blockchain, and Software Prototyping",
+    },
+    {
+      question: "How can I request a quote?",
+      answer:
         "To request a quote, you can fill out the contact form on our website or send us an email with your project requirements.",
-		},
-		{
-			question: "How to arrange the meeting",
-			answer:
-        "On the Home Page, Go to the top there is button of meeting, click that will navigate to fix a meeting.",
-		},
-
-		{
-			question: "What is your pricing structure?",
-			answer:
+    },
+    {
+      question: "How to arrange the meeting",
+      answer:
+        "On the Home Page, Go to the top there is a button for a meeting, click that to navigate to fix a meeting.",
+    },
+    {
+      question: "What is your pricing structure?",
+      answer:
         "Our pricing is tailored to each project based on its complexity, scope, and specific requirements. We provide detailed pricing information after reviewing the project details.",
-		},
-		{
-			question: "Do you provide ongoing maintenance and support?",
-			answer:
+    },
+    {
+      question: "Do you provide ongoing maintenance and support?",
+      answer:
         "Yes, we offer ongoing maintenance and support services to ensure your website or application continues to run smoothly and stays up-to-date.",
-		},
-	];
+    },
+  ];
 
-	return (
-		<Container className="!p-0">
-			<div className="w-full max-w-2xl p-2 mx-auto rounded-2xl">
-				{faqData.map((item, index) => (
-					<div key={index} className="mb-5">
-						<Disclosure>
-							{({ open }) => (
-								<>
-									<Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200">
-										<span>{item.question}</span>
-										{open ? (
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												strokeWidth="1.5"
-												stroke="currentColor"
-												className="w-6 h-6"
-											>
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													d="M6 18L18 6M6 6l12 12"
-												/>
-											</svg>
-										) : (
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												strokeWidth="1.5"
-												stroke="currentColor"
-												className="w-6 h-6"
-											>
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													d="M12 4.5v15m7.5-7.5h-15"
-												/>
-											</svg>
-										)}
-									</Disclosure.Button>
-									<Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-500 dark:text-gray-300 transition-all">
-										{item.answer}
-									</Disclosure.Panel>
-								</>
-							)}
-						</Disclosure>
-					</div>
-				))}
-			</div>
-		</Container>
-	);
+  return (
+    <Container className="!p-0">
+      <div className="w-full max-w-2xl p-2 mx-auto rounded-2xl">
+        {faqData.map((item, index) => (
+          <div key={index} className="mb-5">
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200">
+                    <span>{item.question}</span>
+                    <span className="ml-auto">
+                      {open ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                          />
+                        </svg>
+                      )}
+                    </span>
+                  </Disclosure.Button>
+                  <Transition
+                    show={open}
+                    enter="transition-all duration-500 ease-out"
+                    enterFrom="opacity-0 max-h-0"
+                    enterTo="opacity-100 max-h-full"
+                    leave="transition-all duration-500 ease-out"
+                    leaveFrom="opacity-100 max-h-full"
+                    leaveTo="opacity-0 max-h-0"
+                  >
+                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-500 dark:text-gray-300">
+                      {item.answer}
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+            </Disclosure>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Faq;
--- components/sectionTitle.js
+++ components/sectionTitle.js
@@ -4,36 +4,36 @@ import "aos/dist/aos.css";
 import AnimatedContainer from "./containerAnimated";
 
 const SectionTitle = (props) => {
-	useEffect(() => {
-		AOS.init();
-	}, []);
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-	return (
-		<AnimatedContainer
-			id={props?.id}
-			className={`flex w-full flex-col mt-4 ${
-				props.align === "left" ? "" : "items-center justify-center text-center"
-			}`}
-		>
-			{props.pretitle && (
-				<div className="text-sm font-bold tracking-wider text-indigo-600 uppercase">
-					{props.pretitle}
-				</div>
-			)}
+  return (
+    <AnimatedContainer
+      id={props?.id}
+      className={`flex w-full flex-col mt-4 ${
+        props.align === "left" ? "" : "items-center justify-center text-center"
+      }`}
+    >
+      {props.pretitle && (
+        <h1 className="max-w-2xl font-bold text-3xl tracking-wider text-indigo-600 uppercase lg:text-5xl">
+          {props.pretitle}
+        </h1>
+      )}
 
-			{props.title && (
-				<h2 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
-					{props.title}
-				</h2>
-			)}
+      {props.title && (
+        <h2 className="max-w-2xl mt-3 text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-3xl dark:text-white">
+          {props.title}
+        </h2>
+      )}
 
-			{props.children && (
-				<p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
-					{props.children}
-				</p>
-			)}
-		</AnimatedContainer>
-	);
+      {props.children && (
+        <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
+          {props.children}
+        </p>
+      )}
+    </AnimatedContainer>
+  );
 };
 
 export default SectionTitle;
