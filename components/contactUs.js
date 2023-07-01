5581b6d3922f318651faae3a6d58e4387a72d1bd
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-07-02 02:04:03 +0500

    linting fixed

--- components/contactUs.js
+++ components/contactUs.js
@@ -1,72 +1,72 @@
 import React from "react";
 
 const ContactUsSection = () => {
-  return (
-    <section className="py-16 ">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl text-center font-semibold text-gray-800 dark:text-white mb-8">
+	return (
+		<section className="py-16 ">
+			<div className="container mx-auto px-4">
+				<h2 className="text-3xl text-center font-semibold text-gray-800 dark:text-white mb-8">
           Contact Us
-        </h2>
-        <div className="max-w-3xl mx-auto">
-          <form>
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
-              >
+				</h2>
+				<div className="max-w-3xl mx-auto">
+					<form>
+						<div className="mb-4">
+							<label
+								htmlFor="name"
+								className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
+							>
                 Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-full px-4 py-2 border rounded-md text-gray-800 dark:text-gray-300 focus:outline-none focus:border-indigo-500"
-                placeholder="Enter your name"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
-              >
+							</label>
+							<input
+								type="text"
+								id="name"
+								className="w-full px-4 py-2 border rounded-md text-gray-800 dark:text-gray-300 focus:outline-none focus:border-indigo-500"
+								placeholder="Enter your name"
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="email"
+								className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
+							>
                 Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full px-4 py-2 border rounded-md text-gray-800 dark:text-gray-300 focus:outline-none focus:border-indigo-500"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="message"
-                className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
-              >
+							</label>
+							<input
+								type="email"
+								id="email"
+								className="w-full px-4 py-2 border rounded-md text-gray-800 dark:text-gray-300 focus:outline-none focus:border-indigo-500"
+								placeholder="Enter your email"
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="message"
+								className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2"
+							>
                 Message
-              </label>
-              <textarea
-                id="message"
-                className="w-full px-4 py-2 border rounded-md text-gray-800 dark:text-gray-300 focus:outline-none focus:border-indigo-500"
-                rows="6"
-                style={{
-                  resize: "none",
-                }}
-                placeholder="Enter your message"
-              ></textarea>
-            </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className="px-6 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
-              >
+							</label>
+							<textarea
+								id="message"
+								className="w-full px-4 py-2 border rounded-md text-gray-800 dark:text-gray-300 focus:outline-none focus:border-indigo-500"
+								rows="6"
+								style={{
+									resize: "none",
+								}}
+								placeholder="Enter your message"
+							></textarea>
+						</div>
+						<div className="text-center">
+							<button
+								type="submit"
+								className="px-6 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+							>
                 Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default ContactUsSection;
--- pages/index.js
+++ pages/index.js
@@ -15,78 +15,78 @@ import TeamSection from "../components/team";
 import ContactUsSection from "../components/contactUs";
 
 const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>Technics Lab</title>
-        <meta
-          name="description"
-          content="Quality Precision Deadlines met. Our talented team ensures your project exceeds expectations.
+	return (
+		<>
+			<Head>
+				<title>Technics Lab</title>
+				<meta
+					name="description"
+					content="Quality Precision Deadlines met. Our talented team ensures your project exceeds expectations.
           Expertise across diverse fields. Unleashing desired"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <Navbar />
-      <Hero />
-      <SectionTitle
-        id={"Services"}
-        pretitle="Services"
-        title=" Why should you join us"
-      >
+			<Navbar />
+			<Hero />
+			<SectionTitle
+				id={"Services"}
+				pretitle="Services"
+				title=" Why should you join us"
+			>
         Nextly is a free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
         completely open-source.
-      </SectionTitle>
+			</SectionTitle>
 
-      <Services data={benefitOne} />
-      <SectionTitle
-        id={"Benefits"}
-        pretitle="Benefits"
-        title="Let us Provide you Some more Stuff"
-      >
-        {/* new stuff can be added  */}
-      </SectionTitle>
+			<Services data={benefitOne} />
+			<SectionTitle
+				id={"Benefits"}
+				pretitle="Benefits"
+				title="Let us Provide you Some more Stuff"
+			>
+				{/* new stuff can be added  */}
+			</SectionTitle>
 
-      <Benefits imgPos="right" data={benefitTwo} />
-      <SectionTitle
-        id="Products"
-        pretitle="Watch a video"
-        title="Let us help you with video demonstration"
-      >
-        {/* new stuff can be added  */}
-      </SectionTitle>
+			<Benefits imgPos="right" data={benefitTwo} />
+			<SectionTitle
+				id="Products"
+				pretitle="Watch a video"
+				title="Let us help you with video demonstration"
+			>
+				{/* new stuff can be added  */}
+			</SectionTitle>
 
-      <Video />
-      <SectionTitle
-        id={"Company"}
-        pretitle="Team"
-        title="Every thing is possible with great team"
-      >
-        {/* new stuff can be added  */}
-      </SectionTitle>
+			<Video />
+			<SectionTitle
+				id={"Company"}
+				pretitle="Team"
+				title="Every thing is possible with great team"
+			>
+				{/* new stuff can be added  */}
+			</SectionTitle>
 
-      <TeamSection />
+			<TeamSection />
 
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said"
-      >
+			<SectionTitle
+				pretitle="Testimonials"
+				title="Here's what our customers said"
+			>
         Testimonails is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
-      </SectionTitle>
-      <Testimonials />
+			</SectionTitle>
+			<Testimonials />
 
-      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+			<SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
         Answer your customers possible questions here, it will increase the
         conversion rate as well as support or chat requests.
-      </SectionTitle>
-      <Faq />
-      <ContactUsSection />
-      <Footer />
-      <PopupWidget />
-    </>
-  );
+			</SectionTitle>
+			<Faq />
+			<ContactUsSection />
+			<Footer />
+			<PopupWidget />
+		</>
+	);
 };
 
 export default Home;
