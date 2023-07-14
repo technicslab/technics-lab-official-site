50f3e6550e604d183729856af410dd1c9512338b
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-07-14 10:20:34 +0500

    social links added

---
+++ components/MarketingPartners.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const MarketingPartners = () => {
+  const partners = [
+    {
+      icon: "",
+      name: "",
+    },
+  ];
+  return (
+    <marque>
+      {partners.map((partner, index) => (
+        <Image
+          key={index}
+          src={partner.icon}
+          alt={partner.name}
+          height={40}
+          width={40}
+        />
+      ))}
+    </marque>
+  );
+};
+
+export default MarketingPartners;
--- pages/index.js
+++ pages/index.js
@@ -19,110 +19,111 @@ import UpWidget from "../components/upWidget";
 import { getProjects } from "./api/index";
 
 const Home = ({ projects }) => {
-	return (
-		<div>
-			<Head>
-				<title>Technics Lab</title>
-				<meta
-					name="description"
-					content="Quality Precision Deadlines met. Our talented team ensures your project exceeds expectations.
+  return (
+    <div>
+      <Head>
+        <title>Technics Lab</title>
+        <meta
+          name="description"
+          content="Quality Precision Deadlines met. Our talented team ensures your project exceeds expectations.
           Expertise across diverse fields. Unleashing desired"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Navbar />
-			<div className="lg:p-16 sm:p-4">
-				<Hero />
-				<SectionTitle
-					id={"Services"}
-					pretitle="Services"
-					title=" Why should you join us"
-				>
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <div className="lg:p-16 sm:p-4">
+        <Hero />
+        <SectionTitle
+          id={"Services"}
+          pretitle="Services"
+          title=" Why should you join us"
+        >
           Join us and unlock the potential of these cutting-edge services to
           accelerate your business growth and achieve your goals.
-				</SectionTitle>
+        </SectionTitle>
 
-				<Services data={benefitOne} />
-				<SectionTitle
-					id={"Benefits"}
-					pretitle="Benefits"
-					title="Let us provide you with more value"
-				>
-					{/* new stuff can be added  */}
-				</SectionTitle>
+        <Services data={benefitOne} />
+        <SectionTitle
+          id={"Benefits"}
+          pretitle="Benefits"
+          title="Let us provide you with more value"
+        >
+          {/* new stuff can be added  */}
+        </SectionTitle>
 
-				<Benefits imgPos="right" data={benefitTwo} />
-				<SectionTitle
-					id={"Company-Stats"}
-					pretitle="Company Stats"
-					title="Numbers that Speak: Unleashing the Force of Our Company Stats!"
-				></SectionTitle>
-				<StatsSection />
+        <Benefits imgPos="right" data={benefitTwo} />
+        <SectionTitle
+          id={"Company-Stats"}
+          pretitle="Company Stats"
+          title="Numbers that Speak: Unleashing the Force of Our Company Stats!"
+        ></SectionTitle>
+        <StatsSection />
 
-				<SectionTitle
-					id={"Projects"}
-					pretitle="projects"
-					title="Every thing is possible with great team"
-				></SectionTitle>
+        <SectionTitle
+          id={"Projects"}
+          pretitle="projects"
+          title="Every thing is possible with great team"
+        ></SectionTitle>
 
-				<ProjectSection projects={projects} />
+        {/* {JSON.stringify({ data: "sahi", projects })} */}
+        <ProjectSection projects={projects} />
 
-				<ServicesSection />
+        <ServicesSection />
 
-				<SectionTitle
-					id={"Company"}
-					pretitle="Team"
-					title="Every thing is possible with great team"
-				>
-					{/* new stuff can be added  */}
-				</SectionTitle>
+        <SectionTitle
+          id={"Company"}
+          pretitle="Team"
+          title="Every thing is possible with great team"
+        >
+          {/* new stuff can be added  */}
+        </SectionTitle>
 
-				<TeamSection />
+        <TeamSection />
 
-				<SectionTitle
-					pretitle="Testimonials"
-					title="Here's what our customers said"
-				>
+        <SectionTitle
+          pretitle="Testimonials"
+          title="Here's what our customers said"
+        >
           Testimonails is a great way to increase the brand trust and awareness.
           Use this section to highlight your popular customers.
-				</SectionTitle>
-				{/* <Testimonials testimonialsData={testimonialsData} /> */}
+        </SectionTitle>
+        {/* <Testimonials testimonialsData={testimonialsData} /> */}
 
-				<SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+        <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
           Answer your customers possible questions here, it will increase the
           conversion rate as well as support or chat requests.
-				</SectionTitle>
-				<Faq />
-				<ContactUsSection />
-				<Footer />
-				<UpWidget />
-				<PopupWidget />
-			</div>
-		</div>
-	);
+        </SectionTitle>
+        <Faq />
+        <ContactUsSection />
+        <Footer />
+        <UpWidget />
+        <PopupWidget />
+      </div>
+    </div>
+  );
 };
 export const getServerSideProps = async () => {
-	try {
-		const projects = await getProjects();
-		return {
-			props: {
-				projects,
-				// faqData: faqData,
-			},
-		};
-	} catch (error) {
-		// console.error("Error fetching data:", error);
-		return {
-			props: {
-				data: "any data",
+  try {
+    const projects = await getProjects();
+    console.log(projects, "projects");
+    return {
+      props: {
+        projects,
+      },
+    };
+  } catch (error) {
+    // console.error("Error fetching data:", error);
+    return {
+      props: {
+        data: "any data",
 
-				// faqData: faqData,
-				// projects: projects,
-				// stats,
-				// testimonialsData: null,
-			},
-		};
-	}
+        // faqData: faqData,
+        // projects: projects,
+        // stats,
+        // testimonialsData: null,
+      },
+    };
+  }
 };
 
 export default Home;
